748829444681032cf24ed4c2c496dc75485adf1b
hacker64-pixel <59923822+hacker64-pixel@users.noreply.github.com>
2020-03-07 17:30:59 +0100

    Add files via upload

---
+++ forever.py
@@ -0,0 +1,5 @@
+print("hakiran si")
+import time
+time.sleep(4)
+while True:
+    print("100001000001000001000001000000100000100000")
---
+++ main.py
@@ -0,0 +1,7 @@
+print("e-bay food")
+hrana = input("što želite naručit:")
+adresa = input("koja je vaša adresa:")
+dostava = input("za sporu dostavu 5 $ za brzu dostavu 12 $ a za flash dostavu 50 $:")
+print("želite dobiti "+ hrana +" vaša adresa je "+ adresa + " i dostva je "+ dostava)
+ok = input("da ili ne:")
+import forever
